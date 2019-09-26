f04eccafaeb75eb22a239eb5abbe8e24ab43a316
minatosnow <36951633+minatosnow@users.noreply.github.com>
2019-09-26 16:06:01 +0700

    Merge branch 'dev' of https://github.com/dthung1602/NetworkVisualization into dev
    # Conflicts:
    #	Qt.py

--- Qt.py
+++ Qt.py
@@ -12,8 +12,8 @@ from Stat import Stat
 
 
 class Window(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self):
+        super().__init__()
 
         uic.loadUi('resource/gui/GUI.ui', self)
         self.setWindowIcon(QIcon('resource/gui/icon.ico'))
